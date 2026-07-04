550c36b0bce2ba3fa8763801d75e51ca2c0424e3
RuchitraThiyagaraj <154776996+RuchitraThiyagaraj@users.noreply.github.com>
2026-07-04 19:32:41 +0530

    updated hands on 6

--- DATABASE_INTEGRATION/HandsOn-6/Task1_Setup_ORM.py
+++ DATABASE_INTEGRATION/HandsOn-6/Task1_Setup_ORM.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:your_password@localhost/college_db"
+DATABASE_URL = "mysql+pymysql://root:rootnew@localhost/college_db"
 
 engine = create_engine(DATABASE_URL)
 
